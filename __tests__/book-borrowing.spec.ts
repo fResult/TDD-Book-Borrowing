dfdd90bb9e927837d8799d2981d125dab80f0924
fResult <styxmaz@gmail.com>
2024-03-31 01:01:22 +0700

    it should be `success=false` when book 'other book' which is not in the book store

--- __tests__/book-borrowing.spec.ts
+++ __tests__/book-borrowing.spec.ts
@@ -3,11 +3,24 @@ import { describe, it, expect } from "vitest"
 const bookCollection = ["CLRS", "Element of Programming Style"]
 
 function borrowBook(bookCollection: string[], bookToBorrow: string): any {
-  return bookCollection.filter(book => bookToBorrow !== book)
+  return {
+    success:   bookCollection.find(book => bookToBorrow === book) !== undefined,
+    remainingBooks: bookCollection.filter((book) => bookToBorrow !== book)
+  }
 }
 
 describe("Book borrowing", () => {
-  it("should return ['Element of Programming Style'] when borrow 'CLRS'", () => {
-    expect(borrowBook(bookCollection, "CLRS")).toEqual(["Element of Programming Style"])
+  it("should return ['Element of Programming Style'] and `success=true` when borrow 'CLRS'", () => {
+    expect(borrowBook(bookCollection, "CLRS")).toEqual({
+      success: true,
+      remainingBooks: ["Element of Programming Style"],
+    })
+  })
+
+  it("should be success=false when book 'other book' which is not in the book store", () => {
+    expect(borrowBook(bookCollection, "other book")).toEqual({
+      success: false,
+      remainingBooks: bookCollection,
+    })
   })
 })
