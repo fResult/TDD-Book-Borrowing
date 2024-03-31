c2b85a5e6002ee0abb08fc102f98a188b5fe2187
fResult <styxmaz@gmail.com>
2024-03-31 18:11:12 +0700

    update mock data, test cases, and test results follow the requirements 6

--- __tests__/book-borrowing.spec.ts
+++ __tests__/book-borrowing.spec.ts
@@ -3,26 +3,40 @@ import { borrowBook } from "../src/book-borrowing"
 
 const bookCollection: { title: string; amount: number }[] = [
   { title: "CLRS", amount: 2 },
-  { title: "Element of Programming Styles", amount: 3 },
+  { title: "The Elements of Programming Style", amount: 3 },
+  { title: "How to Design Program", amount: 0 },
 ]
 
-describe("Book borrowing", () => {
+describe("Borrow book `success` as true", () => {
   it("should return success=true and remaining book collection", () => {
     expect(borrowBook(bookCollection, "CLRS")).toEqual({
       success: true,
       remainingBooks: [
         { title: "CLRS", amount: 1 },
-        { title: "Element of Programming Styles", amount: 3 },
+        { title: "The Elements of Programming Style", amount: 3 },
+        { title: "How to Design Program", amount: 0 },
       ],
     })
   })
+})
 
-  it("should return success=false and original book collection with a reason", () => {
+describe("Borrow book `success` as false", () => {
+  it("should return success=false and original book collection with a reason when book is not exist in the store", () => {
     const bookToBorrow = "other book"
 
     expect(borrowBook(bookCollection, bookToBorrow)).toEqual({
       success: false,
       reason: `There is no book [${bookToBorrow}] in this store`,
+      remainingBooks: bookCollection,
+    })
+  })
+
+  it("should return success=false and original book collection with a reason when book is out of stock", () => {
+    const bookToBorrow = "How to Design Program"
+
+    expect(borrowBook(bookCollection, bookToBorrow)).toEqual({
+      success: false,
+      reason: `The book [${bookToBorrow}] is out of stock`,
       remainingBooks: bookCollection,
     })
   })
