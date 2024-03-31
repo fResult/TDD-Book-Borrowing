f3c79232a7eb07269cc838a7c8cdf0c84036a3a8
fResult <styxmaz@gmail.com>
2024-03-31 17:34:23 +0700

    update production code to support requirement 5

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ Practice to TDD mindset
 - [x] 2) It should return the `success` as false and **original book collection** when borrow the book which isn't existed in the book store
 - [x] 3) In addition to the behavior described in requirement 1, it should also return a `success` as true
 - [x] 4) In addition to the behavior described in requirement 2, it should also return the `reason` as "There is no book [«book_name»] in this store"
-- [ ] 5) In addition to the behavior described in requirement 3, it should also return `amount` as original amount - 1
+- [x] 5) In addition to the behavior described in requirement 3, it should also return `amount` as original amount - 1
 - [ ] 6) In addition to the behavior described in requirement 2, it should also return original `amount`
--- src/book-borrowing.ts
+++ src/book-borrowing.ts
@@ -1,5 +1,10 @@
-export function borrowBook(bookCollection: string[], bookToBorrow: string) {
-  const foundBook = bookCollection.find((book) => bookToBorrow === book)
+interface IBookStock {
+  title: string;
+  amount: number;
+}
+
+export function borrowBook(bookCollection: IBookStock[], bookToBorrow: string) {
+  const foundBook = bookCollection.find((book) => bookToBorrow === book.title)
 
   if (!foundBook)
     return {
@@ -10,6 +15,14 @@ export function borrowBook(bookCollection: string[], bookToBorrow: string) {
 
   return {
     success: true,
-    remainingBooks: bookCollection.filter((book) => bookToBorrow !== book),
+    remainingBooks: bookCollection.map(updateBookAmountIfBookExist(bookToBorrow)),
+  }
+}
+
+function updateBookAmountIfBookExist(bookToBorrow: string) {
+  return function forBookAmount(bookStock: IBookStock): IBookStock {
+    return bookToBorrow === bookStock.title ?
+      { ...bookStock, amount: bookStock.amount - 1 }
+      : bookStock
   }
 }
