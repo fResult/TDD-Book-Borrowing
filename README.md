f400324a3d71d94331dbbb752cfbed9ee8755a94
fResult <styxmaz@gmail.com>
2024-04-07 17:36:40 +0700

    update the test case to attach an error message "There is no book [Other Book] in this store"

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Use throwing an error and a `reason` as an **error message** approach
 
 - [x] 1) It should return **remaining books** if the book to borrow is existed in the store
 - [x] 2) It should throw an **error** when borrow the book which isn't existed in the book store
-- [ ] 3) In addition to the behavior described in requirement 2, it should also attach an **error message** as "There is no book [«book_name»] in this store"
+- [x] 3) In addition to the behavior described in requirement 2, it should also attach an **error message** as "There is no book [«book_name»] in this store"
 - [ ] 4) In addition to the behavior described in requirement 3, it should change the **error type** from a normal `Error` to be a `BookNotFoundError`
 - [ ] 5) In addition to the behavior described in requirement 1, it should also return an `amount` as **original amount** - 1
 - [ ] 6) It should throw a `BookOutOfStockError`, with the **error message** as "The book [«book_name»] is out of stock" when that book's `amount` is `0`
--- __tests__/book-borrowing-with-error.spec.ts
+++ __tests__/book-borrowing-with-error.spec.ts
@@ -5,17 +5,15 @@ type Book = string
 const booksInStore = ["CLRS", "The Elements of Programming Style"]
 
 function borrowBook(bookCollection: Book[], bookToBorrow: Book): Book[] {
-  const foundBook = bookCollection.find(book => bookToBorrow === book)
-  if (!foundBook) throw new Error()
+  const foundBook = bookCollection.find((book) => bookToBorrow === book)
+  if (!foundBook) throw new Error(`There is no book [${bookToBorrow}] in this store`)
 
-  return bookCollection.filter(book => bookToBorrow !== book)
+  return bookCollection.filter((book) => bookToBorrow !== book)
 }
 
 describe("Borrow book success", () => {
   it("should return the list [\"The elements of Programming Style\"] when borrow the 'CLRS'", () => {
-    expect(borrowBook(booksInStore, "CLRS")).toEqual([
-      "The Elements of Programming Style",
-    ])
+    expect(borrowBook(booksInStore, "CLRS")).toEqual(["The Elements of Programming Style"])
   })
 
   it("should return the list [\"CLRS\"] when borrow the 'The Elements of Programming Style'", () => {
@@ -24,7 +22,7 @@ describe("Borrow book success", () => {
 })
 
 describe("Borrow book fail with some reasons", () => {
-  it("should throw an error when borrow the book isn't existed in the store", () => {
+  it('should throw an error with message "There is no book [Other Book] in this store"', () => {
     expect(() => borrowBook(booksInStore, "Other Book")).toThrowError()
   })
 })
