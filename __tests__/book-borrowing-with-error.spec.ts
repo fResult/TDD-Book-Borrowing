b365b92aa166736c76dcf2d225ae9ef410795cb4
fResult <styxmaz@gmail.com>
2024-04-07 17:31:18 +0700

    add more test case when borrow other book should throw an `Error`

--- __tests__/book-borrowing-with-error.spec.ts
+++ __tests__/book-borrowing-with-error.spec.ts
@@ -3,6 +3,9 @@ import { describe, expect, it } from "vitest"
 type Book = string
 
 function borrowBook(bookCollection: Book[], bookToBorrow: Book): Book[] {
+  const foundBook = bookCollection.find(book => bookToBorrow === book)
+  if (!foundBook) throw new Error()
+
   return bookCollection.filter(book => bookToBorrow !== book)
 }
 
@@ -15,5 +18,11 @@ describe("Borrow book success", () => {
 
   it("should return the list [\"CLRS\"] when borrow the 'The Elements of Programming Style'", () => {
     expect(borrowBook(["CLRS", "The Elements of Programming Style"], "The Elements of Programming Style")).toEqual(["CLRS"])
+  })
+})
+
+describe("Borrow book fail with some reasons", () => {
+  it("should throw an error when borrow the book isn't existed in the store", () => {
+    expect(() => borrowBook(["CLRS", "The Elements of Programming Style"], "Other Book")).toThrowError()
   })
 })
