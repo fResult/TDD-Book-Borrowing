dc817442d120f8cd910d00d644453d7bfdd87ed9
fResult <styxmaz@gmail.com>
2024-04-07 17:47:47 +0700

    update every test cases using `actual` variable

--- __tests__/book-borrowing-with-error.spec.ts
+++ __tests__/book-borrowing-with-error.spec.ts
@@ -23,17 +23,23 @@ function borrowBook(bookCollection: Book[], bookToBorrow: Book): Book[] {
 
 describe("Borrow book success", () => {
   it("should return the list [\"The elements of Programming Style\"] when borrow the 'CLRS'", () => {
-    expect(borrowBook(booksInStore, "CLRS")).toEqual(["The Elements of Programming Style"])
+    const actual = borrowBook(booksInStore, "CLRS")
+
+    expect(actual).toEqual(["The Elements of Programming Style"])
   })
 
   it("should return the list [\"CLRS\"] when borrow the 'The Elements of Programming Style'", () => {
-    expect(borrowBook(booksInStore, "The Elements of Programming Style")).toEqual(["CLRS"])
+    const actual = borrowBook(booksInStore, "The Elements of Programming Style")
+
+    expect(actual).toEqual(["CLRS"])
   })
 })
 
 describe("Borrow book fail with some reasons", () => {
   it('should throw a `BookNotFoundError` with message "There is no book [Other Book] in this store"', () => {
-    expect(() => borrowBook(booksInStore, "Other Book")).toThrow(BookNotFoundError)
-    expect(() => borrowBook(booksInStore, "Other Book")).toThrow("There is no book [Other Book] in this store")
+    const actual = () => borrowBook(booksInStore, "Other Book")
+
+    expect(actual).toThrow(BookNotFoundError)
+    expect(actual).toThrow("There is no book [Other Book] in this store")
   })
 })
