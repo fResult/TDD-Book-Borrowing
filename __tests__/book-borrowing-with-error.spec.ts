f46d0332fe040fff0143dde7076e36c0b1c66200
fResult <styxmaz@gmail.com>
2024-04-07 17:32:24 +0700

    refactor code in the tests

--- __tests__/book-borrowing-with-error.spec.ts
+++ __tests__/book-borrowing-with-error.spec.ts
@@ -2,6 +2,8 @@ import { describe, expect, it } from "vitest"
 
 type Book = string
 
+const booksInStore = ["CLRS", "The Elements of Programming Style"]
+
 function borrowBook(bookCollection: Book[], bookToBorrow: Book): Book[] {
   const foundBook = bookCollection.find(book => bookToBorrow === book)
   if (!foundBook) throw new Error()
@@ -11,18 +13,18 @@ function borrowBook(bookCollection: Book[], bookToBorrow: Book): Book[] {
 
 describe("Borrow book success", () => {
   it("should return the list [\"The elements of Programming Style\"] when borrow the 'CLRS'", () => {
-    expect(borrowBook(["CLRS", "The Elements of Programming Style"], "CLRS")).toEqual([
+    expect(borrowBook(booksInStore, "CLRS")).toEqual([
       "The Elements of Programming Style",
     ])
   })
 
   it("should return the list [\"CLRS\"] when borrow the 'The Elements of Programming Style'", () => {
-    expect(borrowBook(["CLRS", "The Elements of Programming Style"], "The Elements of Programming Style")).toEqual(["CLRS"])
+    expect(borrowBook(booksInStore, "The Elements of Programming Style")).toEqual(["CLRS"])
   })
 })
 
 describe("Borrow book fail with some reasons", () => {
   it("should throw an error when borrow the book isn't existed in the store", () => {
-    expect(() => borrowBook(["CLRS", "The Elements of Programming Style"], "Other Book")).toThrowError()
+    expect(() => borrowBook(booksInStore, "Other Book")).toThrowError()
   })
 })
